71dc99a42f911b2447473c234e586451a5fff1db
julyrojasm <julerojas1111111@soy.sena.edu.co>
2024-03-31 22:59:47 -0500

    first commit

---
+++ procesos.js
@@ -0,0 +1,98 @@
+function mostrar(){
+    document.getElementById("info").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>El concentrado para perros adultos Chunky con sabor a pollo y arroz será una excelente opción de alimentación saludable para tu perro. Su fuente principal de proteína viene del pollo y es un alimento fabricado  bajo exigentes normas de calidad que proporcionará una nutrición completa y balanceada a tu perro adulto. Gracias a sus altos niveles de nutrientes como vitaminas, minerales, antioxidantes, ácidos grasos omegas, entre otros; Este concentrado contribuirá a la buena salud en general y al mantenimiento de la piel y el pelaje. Además, gracias al DH pack y a su alto nivel de fibra también favorecerá la digestión y la protección del tracto gastrointestinal.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3>2 Kilos, 4 Kilos, 9 Kilos</h3>"  
+}
+
+function mostrar2(){
+    document.getElementById("info2").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Es un concentrado fabricado con carne de verdad y apto para perros adultos de todas las razas. Además, cuenta con ingredientes no alérgicos ideal para aquellos perros con estómagos y pieles sensibles, como el cordero y el salmón que eliminan la posibilidad de irritación y reacciones alérgicas que se pueden presentar en algunos perros frente a otros ingredientes comunes presentes en concentrados regulares. Es un alimento para perros que proporcionará todos los nutrientes necesarios para llevar una dieta balanceada y saludable. Además, gracias al salmón contiene ácidos grasos omegas que favorecerán la salud de la piel y el brillo del pelaje.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1.5 Kilos, 4 Kilos</h3>"  
+}
+
+function mostrar3(){
+    document.getElementById("info3").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Es un alimento ideal para contribuir a una alimentación completa y balanceada que proporcionará los nutrientes adecuados para mantenerlos sanos y activos. Ya que las necesidades nutricionales de los perros de razas pequeñas tienden a ser diferentes, pues por lo general sus estómagos son más sensibles y pequeños. La fórmula de este concentrado cuenta con ácidos grasos, vitaminas, minerales y DH pack que favorecerán su salud en general. Además sus palatables croquetas son más pequeñas con el fin de que sean fáciles de masticar y de disminuir la acumulación de sarro en los dientes.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1.5 Kilos, 4 Kilos, 8 Kilos</h3>"  
+}
+function mostrar4(){
+    document.getElementById("info4").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3> Es un alimento especialmente creado para suplir las necesidades nutricionales de perros cachorros en etapa de crecimiento. Este concentrado tiene un alto nivel de proteína proveniente de carne de pollo real. Su fórmula es rica en nutrientes como vitaminas, minerales, antioxidantes, DH pack y ácidos grasos que favorecerán su salud en general, la digestión, protegerán el tracto gastrointestinal y el mantenimiento óptimo de la piel y el pelaje. Al mismo tiempo que aportarán los niveles de energía necesarios para mantenerlos fuertes y activos.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 2 Kilos, 4 Kilos, 9 Kilos</h3>"  
+}
+function mostrar5(){
+    document.getElementById("info5").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>DeliDog Dent son ricos snacks para perros que además de brindarles una rica experiencia de sabor, también se encargará de cuidar su salud dental. Su especial fórmula y textura favorecerá la limpieza de los dientes, retirando el sarro y la placa acumulada. Además, tiene un rico sabor a menta que proporcionará un aliento fresco. </h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 150gr </h3>"  
+}
+function mostrar6(){
+    document.getElementById("info6").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>El concentrado para perros premium de la línea Professional Line Performance de Max es un concentrado con rico sabor que aportará una nutrición completa y balanceada. Su fórmula posee ingredientes más naturales, libres de  colorantes y sabores artificiales. Es un concentrado cuyo objetivo principal es reforzar la vitalidad de los perros adultos, manteniéndolos activos y llenos de energía. Contiene nutrientes esenciales para una sana nutrición como lo son las proteínas para mantener la masa muscular, antioxidantes para mantener altas las defensas,  vitaminas y minerales.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 2 Kilos</h3>"  
+}
+function mostrar7(){
+    document.getElementById("info7").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>El concentrado para perros adultos de razas pequeñas premium de la línea Professional Line Performance de Max es un concentrado con rico sabor a pollo que aportará una nutrición óptima y balanceada. Su fórmula posee ingredientes naturales, libres de  colorantes, sabores y aromas artificiales. Su objetivo principal es reforzar la vitalidad de los perros adultos de razas pequeñas, manteniéndolos activos y llenos de energía para correr jugar y hacer lo que más les gusta. Contiene nutrienes importantes para llevar una sana nutrición como lo son las proteínas para mantener la masa muscular, antioxidantes para mantener fortalecido el sistema inmunológico,  vitaminas y minerales. Contiene buenas fuentes de ácidos grasos omegas que favorecerán la salud de la piel y el brillo del pelaje.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 2 Kilos</h3>"  
+}
+function mostrar8(){
+    document.getElementById("info8").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>El concentrado para perros cachorros Max Professional Line Performance con sabor a cordero, pollo y arroz será el alimento ideal para proporcionar una alimentación completa y adecuada a perros cachorros en etapa de crecimiento. Su fórmula creada especialmente para esta importante etapa de desarrollo cuenta con proteínas seleccionadas de alta calidad, fuentes de ácidos grasos que serán ideales para fortalecer los músculos durante el crecimiento y la salud de la piel y del pelaje. También cuenta con buena cantidad de calcio que contribuirá al desarrollo de los huesos y de una dentadura sana. </h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 2 Kilos</h3>"  
+}
+function mostrarcat(){
+    document.getElementById("infocat").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Max Cat Adulto es un alimento muy completo y balanceado para gatos adultos, ayuda a reducir el olor de las heces y contribuye a una piel sana y un pelaje hermoso, cuida la salud del tracto urinario de tu mascota. Sin colorantes ni saborizantes artificiales, con taurina para la salud del corazón y la visión, tiene digestibilidad superior al 87%, es antioxidante celular natural y ayuda a mantener sanas las defensas del organismo.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat2(){
+    document.getElementById("infocat2").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Adecuado control del pH urinario, lo que favorece el buen funcionamiento del tracto urinario. Prebiótico y extracto de Yuca que reducen el olor de las heces. Optima digestión y absorción gracias a la inclusion de ingredientes de calidad y aporte de todos los nutrientes esenciales.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat3(){
+    document.getElementById("infocat3").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Además de todo el cariño y atención, ¡necesitas ofrecerle a tu gato una apetitosa comida! Con MAX CAT ADULTOS SABORES - POLLO, PESCADO, ESPINACA Y ZANAHORIA, tu gato estará bien nutrido, feliz y lleno de energía. Relaciones especiales merecen un alimento premium especial.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat4(){
+    document.getElementById("infocat4").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Producido especialmente para que tu gatito crezca sano y fuerte. El nuevo alimento de la línea Professional Line es producido y enriquecido con vitaminas del complejo A, B12, D y E, ayudando directamente en la disminución del olor de las heces, dando un UP en la salud del corazón, y además, siendo ideal para una mejoría del tracto urinario. Y el gusto es inconfundible, extremadamente suave y suculento. ¡A tu gatito le va a encantar!.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat5(){
+    document.getElementById("infocat5").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Contiene extracto de Yucca Schidigera que ayuda al control de las heces, prebióticos y proteínas de alta digestibilidad para la buena digestión del gato, fibras naturales para prevenir la formación de bolas de pelos, taurina para contribuir a la buena visión y audición.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo, 8 Kilos</h3>"  
+}
+function mostrarcat6(){
+    document.getElementById("infocat6").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Alimento ideal para gatos adultos, contiene Taurina contando con una dieta de calidad, llena de aminoácidos adaptados a tu gato, mejorando la ingesta; mejora el desarrollo de la retina y evita enfermedades cardiacas. Contiene Nuggets rellenos que aumentan el sabor de un paladar exigente.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat7(){
+    document.getElementById("infocat7").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>Este alimento tiene el equilibrio perfecto entre leche y Taurina, que mejoran y desarrollan su visión en su crecimiento. Fortalecen su pequeño corazón y construye su organismo. Totalmente Premium, ya que, es libre de colorante y aromatizantes; También es indicado para ser consumido por hembras en sus últimas 3 semanas de gestación y durante toda la etapa de lactancia.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 1 Kilo</h3>"  
+}
+function mostrarcat8(){
+    document.getElementById("infocat8").innerHTML = "<h1> INFORMACION DEL PRODUCTO </h1>"
+    +"<h3>El empaque de esta maravillosa arena Maxicat, es ideal para solidificar las heces y la orina de tu felino, es una arena sanitaria que mejora la absorción, recolección y limpieza de la arenera de tu gato, está hecha para ser manejada de manera fácil, proporciona muy poco polvo, lo que mejora la salud de tu mascota y la tuya al recoger. Al recoger no necesitas cambiar toda su arena si no solo las partes sucias y reemplazar por nueva las partes que se extrajeron. Tu gato la amará.</h3>"
+    +"<h3>Presentaciones Disponible</h3>"
+    +"<h3> 5 Kilos, 10 kilos</h3>"  
+}
